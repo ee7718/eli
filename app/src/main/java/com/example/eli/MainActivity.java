f41b76abe66049fed2b3cce2fc50df195d2d2381
ee7718 <mohammadsaboori232323@gmail.com>
2025-09-27 14:56:49 +0330

    Initial commit

--- app/src/main/java/com/example/eli/MainActivity.java
+++ app/src/main/java/com/example/eli/MainActivity.java
@@ -6,7 +6,6 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
