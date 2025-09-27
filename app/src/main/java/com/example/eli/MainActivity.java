328a2a7fc10beda5d3d8f62c1ecbc2d8e01661de
ee7718 <mohammadsaboori232323@gmail.com>
2025-09-27 14:48:13 +0330

    Initial commit

--- app/src/main/java/com/example/eli/MainActivity.java
+++ app/src/main/java/com/example/eli/MainActivity.java
@@ -6,14 +6,13 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.myButton);
-        button.setOnClickListener(v -> 
-            Toast.makeText(this, "سلام! دکمه زده شد.", Toast.LENGTH_SHORT).show()
-        );
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(v -> Toast.makeText(this, "سلام دکتر!", Toast.LENGTH_SHORT).show());
     }
 }
